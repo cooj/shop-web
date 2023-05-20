56912d810f0d1ea60ebe12beb9ac064f920dce2e
cooj <z8888788@163.com>
2023-05-20 21:13:41 +0800

    chore(deps): 更新依赖，更新部分包对vue3.3特性的更改

--- composables/link.ts
+++ composables/link.ts
@@ -3,15 +3,15 @@
 /**
  * 进入商品页面方法
  */
-export const linkGoodsList = (params: GoodsListParams) => {
+export const linkGoodsList = (params: GoodsListParams): string => {
     const url = '/goods/list'
     const route = useRoute()
     const query = route.query as GoodsListParamsQuery
 
     const data = params.query
     if (params.relate) { // 获取
-    // console.log('Number.isNaN(Number(query.cid)) :>> ', Number.isNaN(Number(query.cid)))
-    // keyword不存在params对象里面时,并且路由里面存在keyword字段时
+        // console.log('Number.isNaN(Number(query.cid)) :>> ', Number.isNaN(Number(query.cid)))
+        // keyword不存在params对象里面时,并且路由里面存在keyword字段时
         if (!('keyword' in params) && 'keyword' in query) data.keyword = query.keyword
 
         // c不存在params对象里面时,并且路由里面存在数字类型cid时
@@ -30,10 +30,11 @@ export const linkGoodsList = (params: GoodsListParams) => {
         const list = Object.keys(data).map(i => `${i}=${encodeURIComponent(data[i as keyof GoodsListParamsQuery] || '')}`)
         return list.length > 0 ? `${url}?${list.join('&')}` : url
     } else {
-        return navigateTo({
+        navigateTo({
             path: url,
             query: data as {},
         })
+        return ''
     }
 
     // return navigateTo({
