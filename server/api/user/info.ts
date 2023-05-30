8b5161e340dcccfc84a0f1815a022f5b055c685c
cooj <z8888788@163.com>
2023-05-30 22:37:04 +0800

    chore(other): server调用外部接口参考示例

--- server/api/user/info.ts
+++ server/api/user/info.ts
@@ -1,6 +1,30 @@
 /**
  * 退出登录
  */
-export default defineEventHandler((event) => {
-    return { code: 200, data: event }
+import { defineEventHandler, getCookie, getHeaders, getMethod, getQuery, readBody } from 'h3'
+
+const config = useRuntimeConfig()
+const baseURL = config.public.apiBaseUrl
+
+export default defineEventHandler(async (event) => {
+    const method = getMethod(event)
+    const params = getQuery(event)
+
+    const headers = getHeaders(event)
+    const authorization = headers.Authorization || getCookie(event, 'auth._token.local')
+
+    const url = event.req.url as string
+
+    const body = method === 'GET' ? undefined : await readBody(event)
+
+    return await $fetch(url, {
+        headers: {
+            'Content-Type': headers['content-type'] as string,
+            'Authorization': authorization as string,
+        },
+        baseURL,
+        method,
+        params,
+        body,
+    })
 })
