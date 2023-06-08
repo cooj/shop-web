acf58ab75d16513c01afdb10facb06d915612a02
cooj <z8888788@163.com>
2023-06-08 14:41:30 +0800

    fix(pages): 首页轮播图、活动专区内容更新

--- layouts/components/header/HeaderBanner.vue
+++ layouts/components/header/HeaderBanner.vue
@@ -3,7 +3,7 @@
     <Swiper class="swp-banner" :slides-per-view="1" :centered-slides="true" :navigation="true" :pagination="true"
         :loop="true" :autoplay="{ delay: 6000,
                                   disableOnInteraction: false }" :speed="1500" effect="fade" :modules="modules">
-        <SwiperSlide v-for="item in bannerList" :key="item.ad_id">
+        <SwiperSlide v-for="item in defData.bannerList" :key="item.ad_id">
             <NuxtLink :to="item.ad_link">
                 <img class="im" :src="item.ad_img" alt="">
             </NuxtLink>
@@ -60,23 +60,30 @@ const modules = [Pagination, Navigation, Autoplay, EffectFade]
 //   initSwiper()
 // })
 
-// 获取导航
-const { data: banner } = await HomeApi.getBanner({ position_id: 1 })
-const bannerList = computed(() => {
-    //     "start_time": 1678334400, //开始时间
-    // "end_time": 1685246400, //结束时间
-    const _list = banner.value?.data.lists.filter((item) => {
-        const nowTime = new Date().getTime() // 当前时间的毫秒数
-        // php时间戳为10位
-        const start = !!(nowTime > item.start_time * 1000 && nowTime < item.end_time * 1000) // 开始和结束时间是否在当前时间范围内 （比如
-        if (item.enabled && start) {
-            return true
-        } else {
-            return false
-        }
-    })
-    return _list || []
+const defData = reactive({
+    bannerList: [] as HomeApi_GetBannerItem[],
 })
+
+// 获取轮播图
+// const getBannerList = async () => {
+const { data: banner } = await HomeApi.getBanner({ position_id: 1 })
+
+//     "start_time": 1678334400, //开始时间
+// "end_time": 1685246400, //结束时间
+const _list = banner.value?.data.lists.filter((item) => {
+    const nowTime = new Date().getTime() // 当前时间的毫秒数
+    // php时间戳为10位
+    const start = !!(nowTime > item.start_time * 1000 && nowTime < item.end_time * 1000) // 开始和结束时间是否在当前时间范围内 （比如
+    if (item.enabled && start) {
+        return true
+    } else {
+        return false
+    }
+})
+defData.bannerList = _list || []
+// }
+
+// getBannerList()
 </script>
 
 <style lang="scss" scoped>
--- layouts/components/header/HeaderIndex.vue
+++ layouts/components/header/HeaderIndex.vue
@@ -24,23 +24,10 @@
                 </div>
             </div>
         </div>
-        <div v-if="isIndex" class="market">
+        <div v-if="isIndex && defData.activeList.length" class="market">
             <div class="grid container">
-                <NuxtLink to="/">
-                    <img class="h150px w100%" src="assets/images/pic/card1.png" alt="">
-                    <!-- <el-image class="h150px w100%" src="assets/images/pic/card1.png" /> -->
-                </NuxtLink>
-                <NuxtLink to="/">
-                    <img class="h150px w100%" src="assets/images/pic/card2.png" alt="">
-                    <!-- <el-image class="h150px w100%" src="assets/images/pic/card1.png" /> -->
-                </NuxtLink>
-                <NuxtLink to="/">
-                    <img class="h150px w100%" src="assets/images/pic/card3.png" alt="">
-                    <!-- <el-image class="h150px w100%" src="assets/images/pic/card1.png" /> -->
-                </NuxtLink>
-                <NuxtLink to="/">
-                    <img class="h150px w100%" src="assets/images/pic/card4.png" alt="">
-                    <!-- <el-image class="h150px w100%" src="assets/images/pic/card1.png" /> -->
+                <NuxtLink v-for="item in defData.activeList" :key="item.ad_id" :to="item.ad_link" target="_blank">
+                    <CoImage class="h150px w100%" :src="item.ad_img" />
                 </NuxtLink>
             </div>
         </div>
@@ -53,6 +40,7 @@ import HeaderMenu from './HeaderMenu.vue'
 
 import HeaderBanner from './HeaderBanner.vue'
 import HeaderUser from './HeaderUser.vue'
+import { HomeApi } from '~/api/home/home'
 
 // import HeaderBanner2 from './HeaderBanner2.vue'
 
@@ -60,6 +48,33 @@ const route = useRoute()
 
 const isIndex = computed(() => {
     return route.path === '/'
+})
+const defData = reactive({
+    activeList: [] as HomeApi_GetBannerItem[],
+})
+
+// 获取活动专区广告
+const getActiveList = async () => {
+    // 获取活动广告
+    const { data: banner } = await HomeApi.getBanner({ position_id: 3 })
+    console.log('banner :>> ', banner)
+    //     "start_time": 1678334400, //开始时间
+    // "end_time": 1685246400, //结束时间
+    const _list = banner.value?.data.lists.filter((item) => {
+        const nowTime = new Date().getTime() // 当前时间的毫秒数
+        // php时间戳为10位
+        const start = !!(nowTime > item.start_time * 1000 && nowTime < item.end_time * 1000) // 开始和结束时间是否在当前时间范围内 （比如
+        if (item.enabled && start) {
+            return true
+        } else {
+            return false
+        }
+    })
+    defData.activeList = _list || []
+}
+
+onMounted(() => {
+    getActiveList()
 })
 </script>
 
