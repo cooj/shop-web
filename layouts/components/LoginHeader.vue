5e7fa9fea1149575afc4a2e4382be5ed53d7deae
cooj <z8888788@163.com>
2023-06-08 16:47:55 +0800

    chore(other): 登录页面背景图压缩，未使用的图片放入assets/image/pic下

--- layouts/components/LoginHeader.vue
+++ layouts/components/LoginHeader.vue
@@ -2,13 +2,16 @@
     <div class="h100px bg-#313131">
         <div class="container">
             <NuxtLink to="/">
-                <img pt3 src="@/assets/images/shop-logo2.png" alt="">
+                <img pt3 :src="systemInfo?.shop_logo" alt="">
             </NuxtLink>
         </div>
     </div>
 </template>
 
 <script setup lang="ts">
+// 获取商城信息
+const useSystem = useSystemState()
+const systemInfo = await useSystem.getSystemInfo()
 </script>
 
 <style scoped>
