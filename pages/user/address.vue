c470e6882e8a9794e51ecdfcdf4b1f95f92799e4
cooj <z8888788@163.com>
2023-07-13 10:16:51 +0800

    fix(pages): 地址管理添加骨架屏渲染，git提交增加粘贴提示

--- pages/user/address.vue
+++ pages/user/address.vue
@@ -1,47 +1,57 @@
 <template>
     <LayoutUser>
-        <el-breadcrumb>
-            <el-breadcrumb-item>
-                账户管理
-            </el-breadcrumb-item>
-            <el-breadcrumb-item>地址管理</el-breadcrumb-item>
-        </el-breadcrumb>
-        <div class="my15px">
-            <el-button type="primary" @click="onAdd">
-                新增地址
-            </el-button>
-        </div>
-        <el-table :data="address?.data" border>
-            <el-table-column prop="contacts" label="收件人" width="150" show-overflow-tooltip />
-            <el-table-column prop="phone" label="联系电话" width="130" show-overflow-tooltip />
-            <el-table-column prop="province" label="收件地址" min-width="150" show-overflow-tooltip>
-                <template #default="{ row }">
-                    {{ setAddressText(row) }}
-                </template>
-            </el-table-column>
-            <el-table-column prop="email" label="邮箱地址" width="150" show-overflow-tooltip />
-            <el-table-column prop="is_default" label="默认地址" width="85" align="center" show-overflow-tooltip>
-                <template #default="{ row }">
-                    <el-tag v-if="row.is_default" type="success">
-                        是
-                    </el-tag>
-                    <el-tag v-else type="info">
-                        否
-                    </el-tag>
-                </template>
-            </el-table-column>
-            <el-table-column prop="operate" label="操作" width="100" align="center" show-overflow-tooltip>
-                <template #default="{ row }">
-                    <el-button type="primary" link size="small" @click="onEdit(row)">
-                        修改
-                    </el-button>
-                    <el-button type="primary" link size="small" @click="onDel(row)">
-                        删除
-                    </el-button>
-                </template>
-            </el-table-column>
-        </el-table>
-        <UserAddressModel ref="modelRef" @update="getAddress" />
+        <el-skeleton :loading="defData.skeleton" animated>
+            <template #template>
+                <div class="pb20px">
+                    <el-skeleton-item class="w20%!" />
+                </div>
+                <div class="min-h500px">
+                    <el-skeleton :rows="5" />
+                </div>
+            </template>
+            <el-breadcrumb>
+                <el-breadcrumb-item>
+                    账户管理
+                </el-breadcrumb-item>
+                <el-breadcrumb-item>地址管理</el-breadcrumb-item>
+            </el-breadcrumb>
+            <div class="my15px">
+                <el-button type="primary" @click="onAdd">
+                    新增地址
+                </el-button>
+            </div>
+            <el-table :data="address?.data" border>
+                <el-table-column prop="contacts" label="收件人" width="150" show-overflow-tooltip />
+                <el-table-column prop="phone" label="联系电话" width="130" show-overflow-tooltip />
+                <el-table-column prop="province" label="收件地址" min-width="150" show-overflow-tooltip>
+                    <template #default="{ row }">
+                        {{ setAddressText(row) }}
+                    </template>
+                </el-table-column>
+                <el-table-column prop="email" label="邮箱地址" width="150" show-overflow-tooltip />
+                <el-table-column prop="is_default" label="默认地址" width="85" align="center" show-overflow-tooltip>
+                    <template #default="{ row }">
+                        <el-tag v-if="row.is_default" type="success">
+                            是
+                        </el-tag>
+                        <el-tag v-else type="info">
+                            否
+                        </el-tag>
+                    </template>
+                </el-table-column>
+                <el-table-column prop="operate" label="操作" width="100" align="center" show-overflow-tooltip>
+                    <template #default="{ row }">
+                        <el-button type="primary" link size="small" @click="onEdit(row)">
+                            修改
+                        </el-button>
+                        <el-button type="primary" link size="small" @click="onDel(row)">
+                            删除
+                        </el-button>
+                    </template>
+                </el-table-column>
+            </el-table>
+            <UserAddressModel ref="modelRef" @update="getAddress" />
+        </el-skeleton>
     </LayoutUser>
 </template>
 
@@ -52,7 +62,14 @@ import { UserAddressApi } from '~/api/user/address'
 
 const modelRef = ref<InstanceType<typeof UserAddressModel>>()
 
+const defData = reactive({
+    skeleton: true, // 骨架屏
+
+})
+
 const { data: address, refresh } = await UserAddressApi.getList() // 获取所有地址列表
+await wait(100)
+defData.skeleton = false // 完成屏幕骨架屏操作后，骨架屏展示页面内容。
 // const addressList = ref(address.value?.data)
 
 // 地址信息拼接
