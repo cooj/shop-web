862462fd14bb9ffdadf2c65586c0c33a7c149567
cooj <z8888788@163.com>
2023-07-19 09:22:12 +0800

    fix(other): 网站关闭优化处理，增加网站开启时，不显示网站关闭页面的判断

--- components/base/BaseClose.vue
+++ components/base/BaseClose.vue
@@ -3,7 +3,7 @@
         <div class="ma max-w100% w600px pt200px">
             <el-alert type="error" show-icon :closable="false">
                 <template #title>
-                    {{ systemStatus.close_msg }}
+                    {{ systemStatus.close_msg || '网站维护中...' }}
                 </template>
             </el-alert>
         </div>
@@ -12,8 +12,4 @@
 
 <script setup lang="ts">
 const { systemStatus } = useSystemState() // 商城状态
-
-// definePageMeta({
-//     layout: 'home',
-// })
 </script>
