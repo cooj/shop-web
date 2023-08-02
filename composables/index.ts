429e36d6b09ff552324588a145388e1251b67690
cooj <z8888788@163.com>
2023-08-02 10:25:27 +0800

    fix(pages): 订单结算页面，新增地址时更新运费

--- composables/index.ts
+++ composables/index.ts
@@ -1,4 +1,2 @@
-// export { useGoodsStore as useGoodsState } from './store/goods'
-
 export { useSystemState, useUserState } from './state/system'
 export { useGoodsState, useCartNumberState, usePayTypeState } from './state/goods'
