385d6a2955fdf45721a34ccf531d3a59ef113465
cooj <z8888788@163.com>
2023-10-11 10:43:15 +0800

    fix(components): 订单确认懒加载，防止数据未获取到；去除接口加密打印内容

--- utils/crypto.ts
+++ utils/crypto.ts
@@ -13,16 +13,11 @@ const { MD5 } = Crypto
  * ```
  */
 export const setSignRule = (secret: string, str: string) => {
-    console.log('bff10842ba8dd8c2631d5b0d96643fd2 :>> ', MD5(secret).toString())
-    console.log('secret :>> ', secret)
-    console.log('time :>> ', str)
     // 第一次加密 （密钥+拼接字符串）进行MD5加密
     const key = secret + str
     const s = MD5(key).toString()
-    console.log('s :>> ', s)
     // 第二次加密 (加密一次后的密钥取前面20个字符+拼接字符串取前面10个字符)进行MD5加密
     const key1 = s.substring(0, 20) + str.substring(0, 10)
     const s1 = MD5(key1).toString()
-    console.log('s1 :>> ', s1)
     return s1
 }
